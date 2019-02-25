aaafd13edb943b8201883874e73641a5cfd33019
Travis CI User <travis@example.org>
2019-02-25 08:33:11 +0000

    Updated via 4ed9e303344a490beb76f32af7d5903f8894a6da @2019-02-25 08:32:19 UTC

---
+++ js/lunr-ui.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+  window.index = new elasticlunr.Index;
+
+  index.saveDocument(false);
+  index.setRef('lunr_index');
+  index.addField('pid');
+  index.addField('title');
+  index.addField('author');
+  index.addField('content');
+  index.addField('_date');
+  index.addField('caption');
+  index.addField('translation');
+  index.addField('keywords');
+
+  function constructCustomResult(item){
+    var link = item.link;
+    var label = '';
+    var thumb = '';
+    var author = '';
+    var meta = [];
+
+    if (item.content) {
+      label = item.title;
+      author = '<b>Exhibit by ' + item.author + '</b><br>';
+      meta.push(item.content.slice(0, 200).replace(/<(?:.|\n)*?>/gm, '').replace(/#|\*|_/gi) + '...');
+    }
+
+    if (item.collection == 'plates') {
+      label = 'Plate #' + item.pid;
+      thumb = '<img class="sq-thumb-sm search-thumb" src="/iiif/images/plates/' + item.pid + '/full/250,/0/default.jpg"/>';
+
+      if (item._date) { meta.push('<b>Date: </b>' + item._date); }
+      // if (item.caption) { meta.push('<b>Caption: </b>' + item.caption); }
+      // if (item.translation) { meta.push('<b>Translation: </b>' + item.translation); }
+      if (item.keywords) { meta.push('<b>Keywords: </b>' + item.keywords); }
+    }
+
+    return '<div class="result"><a href="' + link + '">' + thumb + '<div><span class="item-label">' + label + '</span><br>' + author + meta.join('&nbsp;&nbsp;') + '</div></a></div>';
+  }
+
+
+  $.getJSON("/js/lunr-index.json", function(store) {
+
+    // add docs from json store to index
+    for (i in store) {
+      index.addDoc(store[i]);
+    }
+
+    $('input#search').on('keyup', function() {
+      var results_div = $('#results');
+      var query = $(this).val().normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+      var results = index.search(query, {
+        boolean: 'AND',
+        expand: true
+      });
+      results_div.empty();
+      results_div.prepend("<p><small>Displaying " + results.length + " results.</small></p>");
+      for (var r in results) {
+        var ref = results[r].ref;
+        var result = constructCustomResult(store[ref]);
+        results_div.append(result);
+      }
+    });
+  });
+});
